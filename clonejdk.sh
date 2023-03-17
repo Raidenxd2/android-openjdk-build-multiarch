11ec6d21fbec68f330ce1951dc5b95fc35bbc92f
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2023-03-16 23:06:42 -0400

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 if [ "$TARGET_JDK" == "arm" ]; then
-git clone --depth 1 https://github.com/Raidenxd2/Eclipse-Temurin-OpenJDK8U openjdk
+git clone --depth 1 https://github.com/Raidenxd2/z8mirror openjdk
 elif [ "$BUILD_IOS" == "1" ]; then
 git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 else
 # Use aarch32 repo because it also has aarch64
 
-git clone --depth 1 https://github.com/Raidenxd2/Eclipse-Temurin-OpenJDK8U openjdk
+git clone --depth 1 https://github.com/Raidenxd2/z8mirror openjdk
 fi
